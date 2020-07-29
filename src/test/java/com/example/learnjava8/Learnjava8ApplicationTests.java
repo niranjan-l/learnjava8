fe6c2d10e7112cf0421a29ef248061323e7a1c1b
niranjan-l <raoniranjan007@gmail.com>
2020-07-29 18:57:37 +0530

    Initial commit

---
+++ src/test/java/com/example/learnjava8/Learnjava8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnjava8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Learnjava8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
